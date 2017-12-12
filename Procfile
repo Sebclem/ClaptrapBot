d9b2452b1790c32257e34df62fd01f73e5fb1e74
BrokenFire <seb6596@gmail.com>
2017-12-12 20:08:03 +0100

    *

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dlog4j.configurationFile=/app/Heroku/log4j2.xml -jar /app/build/libs/DiscordBot-0.1.0.jar --server.port=$PORT -t $TOKEN
+web: java -Dlog4j.configurationFile=/app/Heroku/log4j2.xml -jar /app/build/libs/DiscordBot-0.1.0.jar -t $TOKEN
--- src/main/java/net/Broken/BotListener.java
+++ src/main/java/net/Broken/BotListener.java
@@ -23,6 +23,7 @@ import net.dv8tion.jda.core.managers.GuildManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
