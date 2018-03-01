5b8cc93bc5f304d8cf57d1fa8018863af11d6762
Sebastien <seb6596@gmail.com>
2018-03-01 16:04:38 +0100

    Ok my bad

--- DownloadLast.sh
+++ DownloadLast.sh
@@ -3,17 +3,17 @@
 echo "Branch: "$1
 if [[  $1 = "master"  ]]
 then
-	base_url="https://jenkins.seb6596.ovh/job/Bot%20Discord%20Gradle/lastStableBuild/artifact/"
+	base_url="https://jenkins.seb6596.ovh/job/Bot%20Discord%20Gradle/lastStableBuild"
 else
-	base_url="https://jenkins.seb6596.ovh/job/Bot%20Discord%20Gradle%20Devel/lastStableBuild/artifact/"
+	base_url="https://jenkins.seb6596.ovh/job/Bot%20Discord%20Gradle%20Devel/lastStableBuild/"
 fi
 
-data=$(curl -s -g "https://jenkins.seb6596.ovh/job/Bot%20Discord%20Gradle/lastStableBuild/api/xml?xpath=/freeStyleBuild/artifact&wrapper=artifacts")
+data=$(curl -s -g ${base_url}"/api/xml?xpath=/freeStyleBuild/artifact&wrapper=artifacts")
 relativePath=$(grep -oPm1 "(?<=<relativePath>)[^<]+" <<< "$data")
 jarFile=$(grep -oPm1 "(?<=<fileName>)[^<]+" <<< "$data")
 
 
 
-wget ${base_url}${relativePath} -O bot.jar -q
+wget ${base_url}"/artifact/"${relativePath} -O bot.jar -q
 
 
