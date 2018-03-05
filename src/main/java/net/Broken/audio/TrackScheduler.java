270f52321b12ddc9371ff8b8bdb6c054f8b0a61f
Sebastien <seb6596@gmail.com>
2018-03-05 14:55:05 +0100

    Correct null pointer

--- src/main/java/net/Broken/audio/TrackScheduler.java
+++ src/main/java/net/Broken/audio/TrackScheduler.java
@@ -153,7 +153,8 @@ public class TrackScheduler extends AudioEventAdapter {
         // Start the next track, regardless of if something is already playing or not. In case queue was empty, we are
         // giving null to startTrack, which is a valid argument and will simply stop the player.
         UserAudioTrack track = queue.poll();
-        this.currentPlayingTrack = track;
+        if(track != null)
+            this.currentPlayingTrack = track;
         if(track != null)
             player.startTrack(track.getAudioTrack(), false);
         needAutoPlay();
