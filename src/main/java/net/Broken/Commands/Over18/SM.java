63e1d6c216074469d7e3e718f71148c0e2f04c46
Sebastien <seb6596@gmail.com>
2018-05-14 09:28:16 +0200

    Bypass Tumblr safe mode

--- src/main/java/net/Broken/Commands/Over18/SM.java
+++ src/main/java/net/Broken/Commands/Over18/SM.java
@@ -1,28 +1,31 @@
 package net.Broken.Commands.Over18;
 
 import net.Broken.Commande;
+import net.Broken.Tools.FindContentOnWebPage;
 import net.Broken.Tools.Redirection;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
 
 /**
  * TODO Remove this
  */
 public class SM implements Commande {
     Logger logger = LogManager.getLogger();
-    public String HELP="T'es sérieux la?";
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         Redirection redirect= new Redirection();
-        event.getTextChannel().sendMessage(event.getAuthor().getAsMention() + "\n:warning: SM n'est plus disponible pour le moment. Pour plus d'info: https://lc.cx/cbSw :warning: ").queue();
-//            try {
-//                event.getTextChannel().sendMessage(redirect.get("https://bonjourfetish.tumblr.com/random")).queue();
-//            } catch (IOException e) {
-//                logger.warn("Erreur de redirection.");
-//                event.getTextChannel().sendMessage(event.getAuthor().getAsMention() + "\n:warning: **__Erreur de redirection (5 essais), Réessayez__**:warning: ").queue();
-//            }
+        try {
+            String redirectUrl = redirect.get("https://bonjourfetish.tumblr.com/random");
+            String img = FindContentOnWebPage.doYourJob(redirectUrl, "article-picture center", "img");
+            event.getTextChannel().sendMessage(img).queue();
+        } catch (IOException e) {
+            logger.warn("Erreur de redirection.");
+            event.getTextChannel().sendMessage(event.getAuthor().getAsMention() + "\n:warning: **__Erreur de redirection (5 essais), Réessayez__**:warning: ").queue();
+        }
 
     }
 
