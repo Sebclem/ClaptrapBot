985ad35fca927cfd6c53a69a837173ef69f460f2
Sebastien <seb6596@gmail.com>
2018-09-13 10:35:50 +0300

    Add service worker and manifest

---
+++ src/main/resources/static/sw.js
@@ -0,0 +1,36 @@
+
+var CACHE_NAME = 'Clap-Trap-Bot-V1';
+var urlsToCache = [
+    '/',
+    '/music',
+    '/register',
+    '/oauthCallback'
+];
+
+
+
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
