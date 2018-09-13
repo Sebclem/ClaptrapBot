297d137478e6ad50bb13e99a07b9f281729c28ca
Sebastien <seb6596@gmail.com>
2018-09-13 10:58:42 +0300

    Change navbar side

--- src/main/resources/static/js/navabar.js
+++ src/main/resources/static/js/navabar.js
@@ -13,7 +13,7 @@ var nav_name;
 $(document).ready(function() {
     $('#nav-mobile').sidenav({
         menuWidth: 400, // Default is 300
-        edge: 'right', // Choose the horizontal origin
+        edge: 'left', // Choose the horizontal origin
         closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens,
     });
