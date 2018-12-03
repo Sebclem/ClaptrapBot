f216808663f69446c5c8fbe8c112680dd03a0f5f
Sebastien <seb6596@gmail.com>
2018-12-03 15:53:26 +0200

    Correct voice channel show on wrong order on web page

--- src/main/java/net/Broken/audio/GetVoiceChanels.java
+++ src/main/java/net/Broken/audio/GetVoiceChanels.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -53,6 +54,8 @@ public class GetVoiceChanels {
             settingsUtils.cleanVoicePref(guild, pref);
 
         }
+
+        list.sort(Comparator.comparingInt(Channel::getPositionRaw));
 
         return list;
 
