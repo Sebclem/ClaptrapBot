f0d4b865b104d95c11a5543576ab726414899c96
Sebastien <seb6596@gmail.com>
2018-12-31 17:08:22 +0100

    Add time and message count on rank page

---
+++ src/main/java/net/Broken/Tools/TimeConvertor.java
@@ -0,0 +1,29 @@
+package net.Broken.Tools;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TimeConvertor {
+    static Logger logger = LogManager.getLogger();
+    public static ArrayList<String> sToTime(long sec){
+        final int MINUTES_IN_AN_HOUR = 60;
+        final int SECONDS_IN_A_MINUTE = 60;
+
+        int seconds = (int) (sec % SECONDS_IN_A_MINUTE);
+        int totalMinutes = (int) (sec / SECONDS_IN_A_MINUTE);
+        int minutes = totalMinutes % MINUTES_IN_AN_HOUR;
+        int hours = totalMinutes / MINUTES_IN_AN_HOUR;
+        ArrayList<String> list = new ArrayList<>();
+        list.add(String.valueOf(hours));
+        list.add(String.valueOf(minutes));
+        list.add(String.valueOf(seconds));
+
+        return list;
+    }
+}
