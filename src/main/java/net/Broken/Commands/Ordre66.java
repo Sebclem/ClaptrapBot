d131d55aee45b2ca6e73032938e25cecca6716f8
Alexandre64 <alex.cabanne@gmail.com>
2021-01-09 16:00:54 +0100

    Added args to send back only before a date in channelsreview

---
+++ src/main/java/net/Broken/Commands/Ordre66.java
@@ -0,0 +1,43 @@
+package net.Broken.Commands;
+
+import net.Broken.Commande;
+import net.Broken.MainBot;
+import net.Broken.Tools.MessageTimeOut;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.apache.logging.log4j.LogManager;
+
+public class Ordre66 implements Commande {
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+        Message rest = event.getTextChannel().sendMessage("Très bien maître " + event.getAuthor().getAsMention()+". J'arrive ! ").complete();
+        Message reste = event.getTextChannel().sendMessage("https://media2.giphy.com/media/UfzTayIyH7g5hk2BA2/giphy.gif\n").complete();
+    }
+
+    @Override
+    public boolean isPrivateUsable() {
+        return true;
+    }
+
+    @Override
+    public boolean isAdminCmd() {
+        return false;
+    }
+
+    /**
+     * Determines if the command is usable only by bot level admin user
+     *
+     * @return boolean
+     */
+    @Override
+    public boolean isBotAdminCmd() {
+        return false;
+    }
+
+    @Override
+    public boolean isNSFW() {
+        return false;
+    }
+}
