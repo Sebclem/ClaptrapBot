1f34d041b78a35b6758de7aa2e41ad2fe247ede8
SebClem <seb6596@gmail.com>
2022-05-17 22:43:58 +0200

    Build auth provider

---
+++ src/main/java/net/Broken/Api/Security/DiscordAuthenticationProvider.java
@@ -0,0 +1,18 @@
+package net.Broken.Api.Security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class DiscordAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        authentication.getCredentials()
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
