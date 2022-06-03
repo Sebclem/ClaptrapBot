e134459ca2bc7c72e3629bb13f46c8ed3f0b5936
SebClem <seb6596@gmail.com>
2022-06-03 14:28:12 +0200

    :hammer: Add invite link api

--- src/main/java/net/Broken/Api/Controllers/GuildController.java
+++ src/main/java/net/Broken/Api/Controllers/GuildController.java
@@ -1,8 +1,11 @@
 package net.Broken.Api.Controllers;
 
 import net.Broken.Api.Data.Guild;
+import net.Broken.Api.Data.InviteLink;
 import net.Broken.Api.Security.Data.JwtPrincipal;
 import net.Broken.Api.Services.GuildService;
+import net.Broken.MainBot;
+import net.dv8tion.jda.api.Permission;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,5 +29,11 @@ public class GuildController {
     public List<Guild> getMutualGuilds(Authentication authentication){
         JwtPrincipal jwtPrincipal = (JwtPrincipal) authentication.getPrincipal();
         return guildService.getMutualGuilds(jwtPrincipal.user());
+    }
+
+    @GetMapping("inviteLink")
+    public InviteLink getInviteLink(){
+        String link = MainBot.jda.getInviteUrl(Permission.ADMINISTRATOR);
+        return new InviteLink(link);
     }
 }
